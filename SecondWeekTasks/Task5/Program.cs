bb54738a7fc967ef5b07c4df6cd9ac79eb488f0d
GulchexrasGithub <burgulchexra@gmail.com>
2023-01-09 00:23:00 +0500

    SECOND WEEK: Task5

---
+++ SecondWeekTasks/Task5/Program.cs
@@ -0,0 +1,25 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Gulchexa's Program for .NET Girls Community
+// FREE TO USE TO DO TASK
+// ---------------------------------------------------------------
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        int[] MyArray = { 1, 2, 5, 6, 7, 8, };
+        int sum = SumElements(MyArray);
+        Console.WriteLine($"The sum of the elements is: {sum}");
+        Console.ReadKey();
+    }
+
+    public static int SumElements(int[] arr)
+    {
+        int sum = 0;
+        for (int i = 0; i < arr.Length; i++)
+        {
+            sum += arr[i];
+        }
+        return sum;
+    }
+}
