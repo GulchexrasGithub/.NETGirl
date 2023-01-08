f6c6290b3f0a9543325c8e687499fa79c861f76f
GulchexrasGithub <burgulchexra@gmail.com>
2023-01-09 02:53:14 +0500

    SECOND WEEK: Task 14

---
+++ SecondWeekTasks/Task14/Program.cs
@@ -0,0 +1,26 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Gulchexa's Program for .NET Girls Community
+// FREE TO USE TO DO TASK
+// ---------------------------------------------------------------
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        long[] digits = GetDigits(0110864814498415454);
+        Console.WriteLine(string.Join(", ", digits));
+        Console.ReadKey();
+    }
+
+    public static long[] GetDigits(long number)
+    {
+        List<long> digits = new List<long>();
+        while (number > 0)
+        {
+            digits.Add(number % 10);
+            number /= 10;
+        }
+        digits.Reverse();
+        return digits.ToArray();
+    }
+}
