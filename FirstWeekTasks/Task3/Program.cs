afd24c203a4e219d4eb7e0af6546b2dd7e73584d
GulchexrasGithub <burgulchexra@gmail.com>
2023-01-08 15:43:12 +0500

    FIRSTWEEK: Task3

---
+++ FirstWeekTasks/Task3/Program.cs
@@ -0,0 +1,28 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Gulchexa's Program for .NET Girls Community
+// FREE TO USE TO DO TASK
+// ---------------------------------------------------------------
+
+namespace Greeter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "";
+
+            Console.WriteLine("What is your name?");
+            name = Console.ReadLine()!;
+
+            if (name == "Alice" || name == "Bob")
+            {
+                Console.WriteLine($"Hi {name}!");
+            }
+            else
+            {
+                Console.WriteLine("Hi!");
+            }
+            Console.ReadKey();
+        }
+    }
+}
