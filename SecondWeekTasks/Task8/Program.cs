b1c4bf5a9d36f9b4c5cdfc7341a54b75f7b2adb7
GulchexrasGithub <burgulchexra@gmail.com>
2023-01-09 01:37:49 +0500

    SECOND WEEK: Task 8

---
+++ SecondWeekTasks/Task8/Program.cs
@@ -0,0 +1,22 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Gulchexa's Program for .NET Girls Community
+// FREE TO USE TO DO TASK
+// ---------------------------------------------------------------
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
+        OnAll(numbers, x => Console.WriteLine($"{x} * {x} = {x * x}"));
+        Console.ReadKey();
+    }
+
+    public static void OnAll(int[] arr, Action<int> func)
+    {
+        for (int i = 0; i < arr.Length; i++)
+        {
+            func(arr[i]);
+        }
+    }
+}
