5a32e683d37bb41fcd0a03e18a2efc1716724448
GulchexrasGithub <burgulchexra@gmail.com>
2023-01-08 23:48:56 +0500

    SECOND WEEK: Task 3

---
+++ SecondWeekTasks/Task3/Program.cs
@@ -0,0 +1,27 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Gulchexa's Program for .NET Girls Community
+// FREE TO USE TO DO TASK
+// ---------------------------------------------------------------
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        int[] myArray = { 1, 2, 3, 4, 5 };
+        bool elementExists = ElementOccursInArray(myArray, 5);
+        Console.WriteLine(elementExists);
+        Console.ReadKey();
+
+    }
+    public static bool ElementOccursInArray(int[] arr, int element)
+    {
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if (arr[i] == element)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
