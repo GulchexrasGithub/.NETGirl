5ac6202680f9029e426129806f300ad64294421e
GulchexrasGithub <burgulchexra@gmail.com>
2023-01-08 15:33:05 +0500

    FIRST WEEK: Task2

---
+++ FirstWeekTasks/Task2/Program.cs
@@ -0,0 +1,20 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Gulchexa's Program for .NET Girls Community
+// FREE TO USE TO DO TASK
+// ---------------------------------------------------------------
+
+using System;
+
+namespace GreetUser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello " + name);
+            Console.ReadKey();
+        }
+    }
+}
