abd95fffb37d0d3e168b6aed21828c52dfd90c34
GulchexrasGithub <burgulchexra@gmail.com>
2023-01-08 16:28:48 +0500

    FIRSTWEEK: Task4

---
+++ FirstWeekTasks/Task4/Program.cs
@@ -0,0 +1,22 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Gulchexa's Program for .NET Girls Community
+// FREE TO USE TO DO TASK
+// ---------------------------------------------------------------
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Please enter a number: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        int sum = 0;
+
+        for (int i = 1; i <= n; i++)
+        {
+            sum += i;
+        }
+        Console.WriteLine("The sum of the numbers 1 to " + n + " is " + sum);
+        Console.ReadKey();
+    }
+}
