b7c7f85ddf2247a1d15de14e2c2c64e3ab9cf2b9
GulchexrasGithub <burgulchexra@gmail.com>
2023-01-08 22:56:47 +0500

    FIRSTWEEK: Task10

---
+++ FirstWeekTasks/Task10/Program.cs
@@ -0,0 +1,29 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Gulchexa's Program for .NET Girls Community
+// FREE TO USE TO DO TASK
+// ---------------------------------------------------------------
+
+namespace LeapYears
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Next 20 leap years: ");
+
+            int currentYear = DateTime.Now.Year;  // Get the current year
+            int numLeapYears = 0;
+            while (numLeapYears < 20)
+            {
+                if (DateTime.IsLeapYear(currentYear))
+                {
+                    Console.Write(currentYear + " ");
+                    numLeapYears++;
+                }
+                currentYear++;
+            }
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+}
